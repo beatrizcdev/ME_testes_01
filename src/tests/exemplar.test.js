f0c02ad1b0f75aa89518d31e7133ea093b3fdb0d
beatrizcdev <beatrizdecastrom98@outlook.com>
2025-10-01 10:38:45 -0300

    Alteração de nomeclatura

--- src/tests/exemplar.test.js
+++ src/tests/exemplar.test.js
@@ -4,12 +4,13 @@ describe('CT021 Disponibiliza o exemplar e retorna status de sucesso', () => {
 
     let idExemplar = 1
 
-    it('Altera o status de 0 para 1', () => {
+    test('Altera o status de 0 para 1', () => {
         //Arrange
         const livro = new Exemplar(idExemplar)
+        let resultado
 
         //Act
-        const resultado = livro.alterarStatus(1);
+        resultado = livro.alterarStatus(1);
 
         //Assert
         expect(resultado).toBeTruthy();
