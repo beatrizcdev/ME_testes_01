7d363e58bd15b3f39bd6727df0f0d347caf2d1fa
beatrizcdev <beatrizdecastrom98@outlook.com>
2025-10-01 08:48:13 -0300

    comentarios

--- src/tests/exemplar.test.js
+++ src/tests/exemplar.test.js
@@ -5,10 +5,13 @@ describe('CT021 Disponibiliza o exemplar e retorna status de sucesso', () => {
     let idExemplar = 1
 
     it('Altera o status de 0 para 1', () => {
+        //Arrange
         const livro = new Exemplar(idExemplar)
-        idExemplar++
 
+        //Act
         const resultado = livro.alterarStatus(1);
+
+        //Assert
         expect(resultado).toBeTruthy();
         expect(livro.status).toBe(1)
     });
