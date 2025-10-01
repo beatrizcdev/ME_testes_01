49abaab51b528ce95a888a6729247a8a1a64b3ea
beatrizcdev <beatrizdecastrom98@outlook.com>
2025-09-30 21:53:43 -0300

    construtor da classe exemplar e método de status

--- src/models/exemplar.js
+++ src/models/exemplar.js
@@ -1,20 +1,71 @@
-export default class Exemplar{
+import IllegalArgumentException from "./IllegalArgumentException";
 
-    constructor(idExemplar){
-        this.idExemplar = idExemplar
-        this.status = 0
-        this.qtdeEmprestimos = 0
+export default class Exemplar {
+  constructor(idExemplar) {
+    this._idExemplar = idExemplar;
+    this._status = 0;
+    this._qtdeEmprestimos = 0;
+  }
+
+  get idExemplar() {
+    return this._idExemplar;
+  }
+
+  get status() {
+    return this._status;
+  }
+
+  get qtdeEmprestimos() {
+    return this._qtdeEmprestimos;
+  }
+
+  async alterarStatus(status) {
+    switch (this._status) {
+      case 4:
+      case 9:
+        throw new Error(
+          "Exemplar em status final não permite nenhuma operação"
+        );
+
+      case 0:
+      case 2:
+        this.status = 1;
+        break;
+
+      case 1:
+        if (novoStatus === 2 || novoStatus === 5 || novoStatus === 9) {
+          this.status = novoStatus;
+        } else {
+          throw new IllegalArgumentException(
+            "Novo status inválido para o status atual"
+          );
+        }
+        break;
+
+      case 3:
+        if (status === 4) {
+          this.status = status;
+        } else {
+          throw new IllegalArgumentException(
+            "Novo status inválido para o status atual"
+          );
+        }
+        break;
+
+      case 5:
+        if (novoStatus === 1 || novoStatus === 2) {
+          this.status = novoStatus;
+        } else {
+          throw new IllegalArgumentException(
+            "Novo status inválido para o status atual"
+          );
+        }
+        break;
+
+      default:
+        return 0;
     }
 
-    get idExemplar(){
-        return this.idExemplar
-    }
-
-    get status(){
-        return this.status
-    }
-
-    get qtdeEmprestimos(){
-        return this.qtdeEmprestimos
-    }
+    return this.status;
+  }
 }
