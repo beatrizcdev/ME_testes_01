26e7568ddfde6453f55e60c454d2212ab58c91e9
beatrizcdev <beatrizdecastrom98@outlook.com>
2025-10-05 13:03:39 -0300

    ajuste no teste status pra usar o set

--- src/tests/exemplar.test.js
+++ src/tests/exemplar.test.js
@@ -1,45 +1,42 @@
 import Exemplar from "../models/exemplar";
 
-describe('CT021 Disponibiliza o exemplar e retorna status de sucesso', () => {
+describe("CT021 Disponibiliza o exemplar e retorna status de sucesso", () => {
+  let idExemplar = 1;
 
-    let idExemplar = 1
+  test("Altera o status de 0 para 1", () => {
+    //Arrange
+    const livro = new Exemplar(idExemplar);
+    let resultado;
 
-    test('Altera o status de 0 para 1', () => {
-        //Arrange
-        const livro = new Exemplar(idExemplar)
-        let resultado
+    //Act
+    resultado = livro.alterarStatus(1);
 
-        //Act
-        resultado = livro.alterarStatus(1);
-
-        //Assert
-        expect(resultado).toBeTruthy();
-        expect(livro.status).toBe(1)
-    });
+    //Assert
+    expect(resultado).toBeTruthy();
+    expect(livro.status).toBe(1);
+  });
 });
 
-describe('CT041 Descartar um exemplar perdido', () => {
+describe("CT041 Descartar um exemplar perdido", () => {
+  let idExemplar = 1;
 
-    let idExemplar = 1
+  test("Retorna exemplar em status final", async () => {
+    //Arrange
+    const livro = new Exemplar(idExemplar);
+    livro.status = 4;
+    let resultado;
 
-    test('Retorna exemplar em status final', async () => {
-        //Arrange
-        const livro = new Exemplar(idExemplar)
-        await livro.alterarStatus(1)
-        await livro.alterarStatus(2)
-        await livro.alterarStatus(3)
-        await livro.alterarStatus(4)
-        let resultado
+    //Act
+    try {
+      resultado = await livro.alterarStatus(9);
+    } catch (e) {
+      resultado = e.message;
+    }
 
-        //Act
-        try {
-            resultado = await livro.alterarStatus(9);
-        } catch (e) {
-            resultado = e.message;
-        }
-
-        //Assert
-        expect(resultado).toBe("Exemplar em status final não permite nenhuma operação");
-        expect(livro.status).toBe(4)
-    });
+    //Assert
+    expect(resultado).toBe(
+      "Exemplar em status final não permite nenhuma operação"
+    );
+    expect(livro.status).toBe(4);
+  });
 });
