a5d0166e6f0cf8519d386b1a6135ea7b0ad24c31
beatrizcdev <beatrizdecastrom98@outlook.com>
2025-10-06 16:58:08 -0300

    ajuste na identação

--- src/tests/exemplar.test.js
+++ src/tests/exemplar.test.js
@@ -17,6 +17,24 @@ describe("CT021 Disponibiliza o exemplar e retorna status de sucesso", () => {
   });
 });
 
+describe('CT022 Disponibilizar um exemplar que está reservado', () => {
+
+    let idExemplar = 1
+
+    test('Alterar status para 1 de um livro com Status 2', async () => {
+        //Arrange
+        const livro = new Exemplar(idExemplar)
+        livro.status = 2
+
+        //Act
+        let resultado  = await livro.alterarStatus(1)
+ 
+        //Assert
+        expect(resultado).toBeTruthy();
+        expect(livro.status).toBe(1)
+    });
+});
+
 describe("CT041 Descartar um exemplar perdido", () => {
   let idExemplar = 1;
 
@@ -39,22 +57,4 @@ describe("CT041 Descartar um exemplar perdido", () => {
     );
     expect(livro.status).toBe(4);
   });
-});
-
-describe('CT022 Disponibilizar um exemplar que está reservado', () => {
-
-    let idExemplar = 1
-
-    test('Alterar status para 1 de um livro com Status 2', async () => {
-        //Arrange
-        const livro = new Exemplar(idExemplar)
-        livro.status = 2
-
-        //Act
-        let resultado  = await livro.alterarStatus(1)
- 
-        //Assert
-        expect(resultado).toBeTruthy();
-        expect(livro.status).toBe(1)
-    });
 });
